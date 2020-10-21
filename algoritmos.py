5a819672c5a86d1539dc192510c64e8ba0db0c19
Brandy Medina <brandy.medina4640@alumnos.udg.mx>
2020-10-21 14:09:46 -0500

    distancia

--- algoritmos.py
+++ algoritmos.py
@@ -1,6 +1,6 @@
 import math
 
 def distancia_euclidiana(x_1, y_1, x_2, y_2):
-    distan = math.sqrt((x_1-x_2)(x_1-x_2)+(y_1-y_2)(y_1-y_2))
+    distan = math.sqrt((x_1-x_2)**2+(y_1-y_2)**2)
     return distan
     
